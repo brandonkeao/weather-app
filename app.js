9a53168caba32551898d80629eb40d86e58dbc8b
Brandon Keao <Brandon.Keao@gmail.com>
2018-09-11 14:41:50 -0600

    Some updates to error handling

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+  var address = encodeURIComponent(argv['address']);
+
+  console.log(address);
 
 request({
-  url: `http://www.mapquestapi.com/geocoding/v1/address?key=GWGfbCANMxzsupJWT3JUcYa6KbXsPuuo&location==%203927%20wolff%20st.%20denver%20co`,
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=GWGfbCANMxzsupJWT3JUcYa6KbXsPuuo&location==${address}`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
-  console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-  console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+  if(error) {
+    console.log(`Unable to connect to Google servers.`);
+  }
+  try
+  {
+      console.log(`Location: ${JSON.stringify(body.results[0].locations[0])}`);
+      console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+      console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+  } catch (error) { //else if (body.status === 'OK') {
+    console.log(`Unable to find that address. \n\nError: ${error}`);
+  }
 });
